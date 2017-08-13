18194e3760901a47f16e39fbf7cdfa114a8c8c6f
Chris Tan <christanity@gmail.com>
2017-08-13 23:27:19 +1000

    added configuration bash script

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install npm
+npm cache clean -f
+npm install -g n
+n stable
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get update
+apt-get install yarn
